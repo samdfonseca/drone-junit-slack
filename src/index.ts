a03785ce8cdf528400b8ef3b671f3e16fec28fe6
Sam Fonseca <sam.fonseca@nytimes.com>
2018-11-26 09:40:12 -0500

    make message building more testable

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,27 @@ import { parseFile } from './parser'
 import { getChannelId, getWebClient } from './slack'
 import { Opts } from './opts'
 
-function sendTestSuitesSlackMessage(channel: string, suites: TestSuites): Promise<any> {
-  return getWebClient()
-    .chat.postMessage({ channel, ...suites.toSlackMessage() })
-    .then(() => {
-      return Promise.all(
-        (suites.testsuites || [])
-          .filter((suite) => suite.is_failed)
-          .map((suite) => {
-            return getWebClient().chat.postMessage({
-              channel,
-              ...suite.toSlackMessage(),
-            })
-          }),
-      )
-    })
+export function buildTestSuitesSlackMessages(channel: string, suites: TestSuites): any[] {
+  return [
+    { channel, ...suites.toSlackMessage() },
+    ...(suites.testsuites || [])
+      .filter((suite) => suite.is_failed)
+      .map((suite) => {
+        return { channel, ...suite.toSlackMessage() }
+      }),
+  ]
 }
 
-export function main() {
+export async function main() {
   const opts = new Opts()
-  getChannelId(opts.channel).then((channel_id) => {
-    return parseFile(opts.junit_file).then((suites) =>
-      sendTestSuitesSlackMessage(channel_id, suites),
-    )
+  const channel_id = await getChannelId(opts.channel)
+  const suites = await parseFile(opts.junit_file)
+  const messages = buildTestSuitesSlackMessages(channel_id, suites)
+  messages.forEach(async (message) => {
+    try {
+      await getWebClient().chat.postMessage(message)
+    } catch (err) {
+      console.error(err)
+    }
   })
 }
